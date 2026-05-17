0551ec551d98137498701d1aef4d373982abb024
Gypen Florian <la215853@student.helha.be>
2026-05-17 04:08:02 +0200

    Ajout du fond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import cv2
 
 # Constante du pendule
 L1 = 1.0
@@ -25,6 +25,11 @@ def calculate_derivates(etat, t):
     a1 = (m2 * L1 * omega1**2 * np.sin(delta) * np.cos(delta) + m2 * g * np.sin(theta2) * np.cos(delta) + m2 * L2 * omega2**2 * np.sin(delta) - (m1 + m2) * g * np.sin(theta1)) / den1
     a2 = (-m2 * L2 * omega2**2 * np.sin(delta) * np.cos(delta) + (m1 + m2) * g * np.sin(theta1) * np.cos(delta) - (m1 + m2) * L1 * omega1**2 * np.sin(delta) - (m1 + m2) * g * np.sin(theta2)) / den2
     
-
+    return [omega1, omega2, a1, a2]
     
+def draw_pendule(theta1, theta2):
+    
+    # Dessiner le fond (en noire pour faciliter l'entrainement du modèle)
+    image = p.zeros((64,64), dtype=np.uint8) # uint8 signifie qu'on utilise des nombres entre 0 et 255 pour représenter les niveaux de gris
+
     
